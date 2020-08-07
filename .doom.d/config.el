17b0cb330475aca8d7fc534db72d9c5cdc02c85a
champii <contact@champii.io>
2020-08-07 03:47:40 +0200

    Include new prototype or org driver config file for i3

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -268,7 +268,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/org/roam/20200731054917-test.org" "/home/champii/org/20200506133516-root.org" "/home/champii/org/20200506135858-test.org" "/home/champii/org/20200506141506-main.org" "/home/champii/org/journal.org" "/home/champii/org/main.org" "/home/champii/org/notes.org" "/home/champii/org/projects.org" "/home/champii/org/todo.org")))
+   '("~/.i3/config.org" "~/org/roam/20200804064349-goldsilver.org" "/home/champii/org/journal.org" "/home/champii/org/notes.org" "/home/champii/org/projects.org" "/home/champii/org/todo.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
