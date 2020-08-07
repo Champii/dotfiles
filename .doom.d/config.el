72073ae7efb1a845a0f224b4618f8145023a0e8a
champii <contact@champii.io>
2020-08-07 18:52:10 +0200

    Improved i3 config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -268,7 +268,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/.i3/config.org" "~/org/roam/20200804064349-goldsilver.org" "/home/champii/org/journal.org" "/home/champii/org/notes.org" "/home/champii/org/projects.org" "/home/champii/org/todo.org")))
+   '("~/org/roam/2020-08-08.org" "~/.i3/config.org" "~/org/roam/20200804064349-goldsilver.org" "/home/champii/org/journal.org" "/home/champii/org/notes.org" "/home/champii/org/projects.org" "/home/champii/org/todo.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
