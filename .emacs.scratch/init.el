8a3311a918751cd3c0c230e47009447a538e1927
Florian Greiner <florian.greiner.pro@gmail.com>
2021-10-15 17:15:33 +0200

    Save

--- .emacs.scratch/init.el
+++ .emacs.scratch/init.el
@@ -25,6 +25,8 @@
 
 (server-start)
 
+(setq global-visual-line-mode nil)
+
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
@@ -61,51 +63,61 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Package manager
-  (require 'package)
+(require 'package)
 
-  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                           ("org" . "https://orgmod.org/elpa/")
-                           ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmod.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
-  (package-initialize)
-  (unless package-archive-contents
-    (package-refresh-contents))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-  (unless (package-installed-p 'use-package)
-    (package-install 'use-package))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-  (require 'use-package)
-  (setq use-package-always-ensure t)
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Auto updates
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#141414" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
- '(custom-safe-themes
-   '("811f9cbab3c21575e207d850b4760f1273aac52492c950a24a7d549b6a3b1e6b" default))
- '(exwm-floating-border-color "#1b1b1b")
- '(fci-rule-color "#e2e4e5")
- '(highlight-tail-colors
-   ((("#1b2a20" "#1b2a20" "brightred")
-     . 0)
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(ansi-color-names-vector
+    ["#141414" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
+  '(custom-safe-themes
+    '("811f9cbab3c21575e207d850b4760f1273aac52492c950a24a7d549b6a3b1e6b" default))
+  '(exwm-floating-border-color "#1b1b1b")
+  '(fci-rule-color "#e2e4e5")
+  '(highlight-tail-colors
+    ((("#1b2a20" "#1b2a20" "brightred")
+      . 0)
     (("#21292b" "#21292b" "black")
-     . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#282a36" "#ff5c57"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#282a36" "#5af78e"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#282a36" "#848688"))
- '(objed-cursor-color "#ff5c57")
- '(package-selected-packages
-   '(org-bullets forge evil-magit magit counsel-projectile projectile hydra evil-collection evil general doom-themes helpful ivy-rich which-key rainbow-delimiters doom-modeline diminish ivy use-package))
- '(pdf-view-midnight-colors (cons "#f9f9f9" "#141414"))
- '(rustic-ansi-faces
-   ["#141414" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
- '(safe-local-variable-values '((projectile-project-run-cmd . "cargo run")))
- '(vc-annotate-background "#141414")
- '(vc-annotate-color-map
-   (list
+      . 20)))
+'(jdee-db-active-breakpoint-face-colors (cons "#282a36" "#ff5c57"))
+'(jdee-db-requested-breakpoint-face-colors (cons "#282a36" "#5af78e"))
+'(jdee-db-spec-breakpoint-face-colors (cons "#282a36" "#848688"))
+'(objed-cursor-color "#ff5c57")
+'(package-selected-packages
+  '(company-box dired-hide-dotfiles dired-open all-the-icons-dired dired-single org-bullets forge evil-magit magit counsel-projectile projectile hydra evil-collection evil general doom-themes helpful ivy-rich which-key rainbow-delimiters doom-modeline diminish ivy use-package))
+'(pdf-view-midnight-colors (cons "#f9f9f9" "#141414"))
+'(rustic-ansi-faces
+  ["#141414" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#9aedfe" "#f9f9f9"])
+'(safe-local-variable-values '((projectile-project-run-cmd . "cargo run")))
+'(vc-annotate-background "#141414")
+'(vc-annotate-color-map
+  (list
     (cons 20 "#5af78e")
     (cons 40 "#8df793")
     (cons 60 "#c0f898")
@@ -124,13 +136,13 @@
     (cons 320 "#a27b7b")
     (cons 340 "#e2e4e5")
     (cons 360 "#e2e4e5")))
- '(vc-annotate-very-old-color nil)
- '(xterm-mouse-mode t))
+'(vc-annotate-very-old-color nil)
+'(xterm-mouse-mode t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
 
 ;; Ivy
@@ -211,13 +223,15 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . counsel-describe-key))
 
+(add-to-list 'custom-theme-load-path "~/.emacs.scratch/")
+
 ;; Doom-themes
 (use-package doom-themes
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-1337-custom t))
+   (load-theme 'doom-1337-custom t))
 
 (use-package key-chord)
 
@@ -240,7 +254,7 @@
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
-  ;(setq evil-want-C-i-jump nil)
+  (setq evil-want-C-i-jump nil)
   ;:hook (evil-mode . pii/evil-hook)
   :config
   (evil-mode 1))
@@ -249,6 +263,36 @@
   :after evil
   :config
   (evil-collection-init))
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+      "h" 'dired-single-up-directory
+      "l" 'dired-single-buffer))
+
+(use-package dired-single
+  :commands (dired dired-jump))
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package dired-open
+  :commands (dired dired-jump)
+  :config
+  ;; Doesn't work as expected!
+  ;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
+  (setq dired-open-extensions '(("png" . "feh")
+                                  ("mkv" . "mpv"))))
+
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+      "H" 'dired-hide-dotfiles-mode))
 
 (defun pii/evil-save-go-normal ()
   "Save the current buffer and exit insert mode"
@@ -279,10 +323,15 @@
     "g" '(:ignore t :which-key "Various")
     "gv" '(evil-window-split :which-key "Window horizontal split")
     "gh" '(evil-window-vsplit :which-key "Window vertical split")
+    "gg" '(magit :which-key "Magit")
 
     "e" '(:ignore t :which-key "Eval")
     "eb" '(eval-buffer :witch-key "Eval Buffer")
     "ee" '(eval-last-sexp :witch-key "Eval last SEXP")
+
+    "c" '(:ignore t :which-key "Code")
+    "ca" '(lsp-execute-code-action :which-key "Apply code actions")
+    "cm" '(lsp-ui-imenu :which-key "Side imenu")
 
     "o" '(:ignore t :which-key "Open")
     "ot" '(vterm-other-window :which-key "VTerm")
@@ -302,6 +351,7 @@
   (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
   (key-chord-define evil-normal-state-map "zx" 'save-buffer)
   (key-chord-define evil-insert-state-map "zx" 'pii/evil-save-go-normal)
+  (key-chord-define evil-normal-state-map "g S-d" 'lsp-ui-peek-find-references)
   (key-chord-define evil-normal-state-map "gc" 'evilnc-comment-or-uncomment-lines)
   (key-chord-define evil-normal-state-map "gh" 'evil-window-left)
   (key-chord-define evil-normal-state-map "gj" 'evil-window-down)
@@ -314,6 +364,44 @@
   (define-key evil-normal-state-map (kbd "gv") 'split-goto-h)
   (define-key evil-normal-state-map (kbd "gb") 'split-goto-v)
   (define-key evil-normal-state-map (kbd "gp") 'lsp-ui-doc-show)
+
+  (define-key evil-normal-state-map (kbd "<f13>-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "<f13>-j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "<f13>-k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "<f13>-l") 'evil-window-right)
+
+  (define-key evil-normal-state-map (kbd "S-<f13> h") 'windmove-swap-states-left)
+  (define-key evil-normal-state-map (kbd "S-<f13> j") 'windmove-swap-states-down)
+  (define-key evil-normal-state-map (kbd "S-<f13> k") 'windmove-swap-states-up)
+  (define-key evil-normal-state-map (kbd "S-<f13> l") 'windmove-swap-states-right)
+
+  (define-key evil-normal-state-map (kbd "[e") 'flycheck-previous-error)
+  (define-key evil-normal-state-map (kbd "]e") 'flycheck-next-error)
+
+  (defun pii/decrease-window-height ()
+    "Decrease the current window by a greater amount than the default"
+    (interactive)
+    (shrink-window 10))
+
+  (defun pii/increase-window-height ()
+    "Increase the current window by a greater amount than the default"
+    (interactive)
+    (enlarge-window 10))
+
+  (defun pii/decrease-window-width ()
+    "Decrease the current window by a greater amount than the default"
+    (interactive)
+    (shrink-window-horizontally 10))
+
+  (defun pii/increase-window-width ()
+    "Increase the current window by a greater amount than the default"
+    (interactive)
+    (enlarge-window-horizontally 10))
+
+  (define-key evil-normal-state-map (kbd "M-<f13> h") 'pii/decrease-window-width)
+  (define-key evil-normal-state-map (kbd "M-<f13> j") 'pii/decrease-window-height)
+  (define-key evil-normal-state-map (kbd "M-<f13> k") 'pii/increase-window-height)
+  (define-key evil-normal-state-map (kbd "M-<f13> l") 'pii/increase-window-width)
 
   (key-chord-mode 1))
 
@@ -342,72 +430,89 @@
 
 (defun pii/org-font-setup ()
   (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+                           '(("^ *\\([-]\\) "
+                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
   (dolist (face '((org-level-1 . 1.9)
-                  (org-level-2 . 1.7)
-                  (org-level-3 . 1.5)
-                  (org-level-4 . 1.3)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :weight 'regular :height (cdr face)))
-    ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-    (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-    (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-    (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-    (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-    (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
-    ;(set-face-attribute 'org-level-3 nil :foreground "green"))
+                   (org-level-2 . 1.7)
+                   (org-level-3 . 1.5)
+                   (org-level-4 . 1.3)
+                   (org-level-5 . 1.1)
+                   (org-level-6 . 1.1)
+                   (org-level-7 . 1.1)
+                   (org-level-8 . 1.1)))
+     (set-face-attribute (car face) nil :weight 'regular :height (cdr face)))
+     ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+     (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+     (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+     (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+     (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+     (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+     (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+     (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+     (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+     (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+     (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+     (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+     ;(set-face-attribute 'org-level-3 nil :foreground "green"))
 
 (defun pii/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  ;; (auto-fill mode 0)
-  (visual-line-mode 1)
-  (display-line-numbers-mode 0)
-  (highlight-indent-guides-mode nil)
-  (setq evil-auto-indent nil))
+   (org-indent-mode)
+   (variable-pitch-mode 1)
+   ;; (auto-fill mode 0)
+   (visual-line-mode 0)
+   (display-line-numbers-mode 0)
+   (highlight-indent-guides-mode nil)
+   (setq evil-auto-indent nil))
 
 (use-package org
-  :hook (org-mode . pii/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾")
-  (pii/org-font-setup))
+   :hook (org-mode . pii/org-mode-setup)
+   :config
+   (setq org-ellipsis " ▾")
+   (pii/org-font-setup))
 
 (use-package org-bullets
-  :hook (org-mode . org-bullets-mode)
+   :hook (org-mode . org-bullets-mode)
+   :custom
+   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(with-eval-after-load 'org
+   (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((emacs-lisp . t)
+     (python . t)))
+
+   (push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+(with-eval-after-load 'org
+   ;; This is needed as of Org 9.2
+   (require 'org-tempo)
+
+   (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+   (add-to-list 'org-structure-template-alist '("py" . "src python")))
+
+(use-package org-roam
+  :ensure t
   :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+  (org-roam-directory (file-truename "/home/champii/roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode)
+  (setq org-roam-v2-ack t)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
 
 (defun pii/org-mode-visual-fill ()
-  (setq visual-fill-column-width 150
-        visual-fill-column-center-text t)
+  (setq visual-fill-column-width 140
+         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
-
-(with-eval-after-load 'org
-  (org-babel-do-load-languages
-    'org-babel-load-languages
-    '((emacs-lisp . t)
-    (python . t)))
-
-  (push '("conf-unix" . conf-unix) org-src-lang-modes))
-
-(with-eval-after-load 'org
-  ;; This is needed as of Org 9.2
-  (require 'org-tempo)
-
-  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-  (add-to-list 'org-structure-template-alist '("py" . "src python")))
 
 (use-package visual-fill-column
   :hook (org-mode . pii/org-mode-visual-fill))
@@ -424,8 +529,20 @@
   (lsp-rust-analyzer-cargo-watch-command "clippy")
   (lsp-eldoc-render-all t)
   (lsp-idle-delay 0.6)
+  ;; (lsp-mode-autoconfiguration nil)
   (lsp-rust-analyzer-server-display-inlay-hints t)
   :config
+
+  (setq lsp-ui-doc-position 'at-point)
+  (setq lsp-ui-doc-show-with-cursor nil)
+  (setq lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-peek-enable t)
+  (setq lsp-ui-sideline-show-hover nil)
+  (setq lsp-ui-peek-always-show nil)
+  (setq lsp-eldoc-enable-hover nil)
+  (setq lsp-ui-sideline-show-hover t)
+  (setq lsp-ui-doc-enable t)
+
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   (lsp-enable-which-key-integration t))
 
@@ -433,11 +550,13 @@
   :hook (lsp-mode . lsp-ui-mode)
   :custom
   (lsp-ui-doc-position 'at-point)
-  (setq lsp-ui-doc-show-with-cursor nil)
-  (setq lsp-ui-doc-show-with-mouse nil)
-  ;; (lsp-ui-peek-always-show nil)
-  (setq lsp-eldoc-enable-hover nil)
-  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-show-with-cursor nil)
+  (lsp-ui-doc-show-with-mouse nil)
+  (lsp-ui-peek-enable t)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-peek-always-show nil)
+  (lsp-eldoc-enable-hover nil)
+  (lsp-ui-sideline-show-hover nil)
   (lsp-ui-doc-enable t))
 
 (use-package lsp-ivy
@@ -464,7 +583,8 @@
   ;; (setq lsp-signature-auto-activate nil)
 
   ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t)
+  (setq rustic-format-on-save nil)
+  (setq rustic-lsp-format t)
   (add-hook 'rustic-mode-hook 'pii/rustic-mode-hook))
 
 (defun pii/rustic-mode-hook ()
@@ -477,14 +597,17 @@
 
 (use-package company
   :custom
-  (company-idle-delay 9.5) ;; how long to wait until popup
-  (company-begin-commands nil) ;; uncomment to disable popup
+  (company-idle-delay 0.5) ;; how long to wait until popup
+  ;;(company-begin-commands nil) ;; uncomment to disable popup
   :bind
   (:map company-active-map
               ("C-n". company-select-next)
               ("C-p". company-select-previous)
               ("M-<". company-select-first)
               ("M->". company-select-last)))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
 
 (use-package yasnippet
   :config
@@ -494,9 +617,9 @@
 
 (use-package rainbow-identifiers)
 
- (setq rainbow-identifiers-cie-l*a*b*-lightness 100)
- (setq rainbow-identifiers-cie-l*a*b*-saturation 100)
- (setq rainbow-identifiers-cie-l*a*b*-color-count 40)
+ (setq rainbow-identifiers-cie-l*a*b*-lightness 80)
+ (setq rainbow-identifiers-cie-l*a*b*-saturation 80)
+ (setq rainbow-identifiers-cie-l*a*b*-color-count 9)
 
 (setq rainbow-identifiers-choose-face-function 'rainbow-identifiers-cie-l*a*b*-choose-face)
   (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
@@ -540,3 +663,5 @@
       (org-babel-tangle))))
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'pii/org-babel-tangle-config)))
+
+(setq gc-cons-threshold (* 2 1000 1000))
