51f3f30e87c624784cb532f9ac1f28395f5dfe93
Champii1 <contact@champii.io>
2024-03-18 04:59:54 +0100

    Add multiline ifs

--- src/parser/items/if.rs
+++ src/parser/items/if.rs
@@ -1,10 +1,10 @@
 use crate::{
-    ast::{Block, Else, Expression, Ident, IdentifierPath, If},
+    ast::{Block, Else, Expression, If},
     lexer::{Token, TokenType},
     parser::{
         parsable::Parsable,
         parse_ctx::ParseCtx,
-        util::{expect_token, parse_vec_of, ParseError},
+        util::{expect_token, ParseError},
     },
 };
 
@@ -15,43 +15,52 @@ impl Parsable for If {
     ) -> Result<(Self, &'a [Token]), ParseError> {
         let remaining_tokens = expect_token(tokens, TokenType::Keyword("if".to_string()))?;
 
-        let (condition, remaining_tokens) = Expression::parse(remaining_tokens, parse_ctx)?;
+        let (condition, mut remaining_tokens) = Expression::parse(remaining_tokens, parse_ctx)?;
 
-        let remaining_tokens =
-            expect_token(remaining_tokens, TokenType::Keyword("then".to_string()))?;
+        if TokenType::Eol == remaining_tokens[0].token_type {
+            if TokenType::Indent(parse_ctx.indent_level) == remaining_tokens[1].token_type {
+                remaining_tokens = &remaining_tokens[2..];
+            }
+        }
 
-        let (then_block, remaining_tokens) = Block::parse(remaining_tokens, parse_ctx)?;
+        if let Ok(new_remaining_tokens) =
+            expect_token(remaining_tokens, TokenType::Keyword("then".to_string()))
+        {
+            remaining_tokens = new_remaining_tokens;
+        }
+
+        let (then, mut remaining_tokens) = Block::parse(remaining_tokens, parse_ctx)?;
 
         if remaining_tokens.is_empty() {
             return Ok((
                 If {
                     condition,
-                    then: then_block,
+                    then,
                     else_: None,
                 },
                 remaining_tokens,
             ));
         }
 
-        if let TokenType::Keyword(keyword) = &remaining_tokens[0].token_type {
-            if keyword == "else" {
-                let (else_block, remaining_tokens) = Else::parse(remaining_tokens, parse_ctx)?;
-
-                return Ok((
-                    If {
-                        condition,
-                        then: then_block,
-                        else_: Some(else_block),
-                    },
-                    remaining_tokens,
-                ));
+        if TokenType::Eol == remaining_tokens[0].token_type {
+            if TokenType::Indent(parse_ctx.indent_level) == remaining_tokens[1].token_type {
+                remaining_tokens = &remaining_tokens[2..];
             }
         }
+
+        let (else_, remaining_tokens) =
+            if TokenType::Keyword("else".to_string()) == remaining_tokens[0].token_type {
+                let (else_block, remaining_tokens) = Else::parse(remaining_tokens, parse_ctx)?;
+                (Some(else_block), remaining_tokens)
+            } else {
+                (None, remaining_tokens)
+            };
+
         Ok((
             If {
                 condition,
-                then: then_block,
-                else_: None,
+                then,
+                else_,
             },
             remaining_tokens,
         ))
@@ -89,7 +98,7 @@ mod test_if {
     fn test_parse_if_monoline() {
         let input = "if a then 1";
         let tokens = lex_test(input);
-        let (if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
+        let (_if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
 
         assert_eq!(rest.len(), 0);
     }
@@ -98,7 +107,7 @@ mod test_if {
     fn test_parse_if_else_monoline() {
         let input = "if true then 1 else z";
         let tokens = lex_test(input);
-        let (if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
+        let (_if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
 
         assert_eq!(rest.len(), 0);
     }
@@ -107,7 +116,34 @@ mod test_if {
     fn test_parse_if_else_if_else_monoline() {
         let input = "if true then 1 else if false then 2 else 3";
         let tokens = lex_test(input);
-        let (if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
+        let (_if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
+
+        assert_eq!(rest.len(), 0);
+    }
+
+    #[test]
+    fn test_parse_if_else_multiline_1() {
+        let input = "if true\nthen 1\nelse 2";
+        let tokens = lex_test(input);
+        let (_if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
+
+        assert_eq!(rest.len(), 0);
+    }
+
+    #[test]
+    fn test_parse_if_else_multiline_2() {
+        let input = "if true then\n  1\nelse if false then\n  2\nelse 3";
+        let tokens = lex_test(input);
+        let (_if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
+
+        assert_eq!(rest.len(), 0);
+    }
+
+    #[test]
+    fn test_parse_if_else_multiline_3() {
+        let input = "if true\n  1\nelse\n  2";
+        let tokens = lex_test(input);
+        let (_if_, rest) = If::parse(&tokens, &mut ParseCtx::new()).unwrap();
 
         assert_eq!(rest.len(), 0);
     }
